087c5dfe87d7b58b24a3ffd82dd98730937e98c7
Blindau2 <blindau2@uwm.edu>
2021-02-28 21:25:18 -0600

    Put together tables for the models. Nothing migrated because of interpreter issues

--- Backend/src/api/models.py
+++ Backend/src/api/models.py
@@ -2,6 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Profile(models.Model):
+    bio = models.TextField()
+    profile_picture = models.ImageField()
+
+
+class User(models.Model):
+    username = models.charField(max_length=100)
+    password = models.charField(max_length=256)
+    email = models.EmailField(max_length=100)
+    profile = models.OneToOneField(Profile)
+
+
+class Tag(models.Model):
+    pass
+
+class TransportationTag(Tag):
+    transport_type = models.charField()
+
+
+class LocationTag(Tag):
+    location_name = models.charField()
+
+
+class AccommodationTag(Tag):
+    accommodation_type = models.charField()
+
+
+class Itinerary(models.Model):
+    itinerary_title = models.charField()
+    budget = models.IntegerField()
+    duration_magnitude = models.IntegerField()
+    tag = models.ManyToManyField(Tag)
 
 # Create your models here.
 
