366b81e1c460be4688f5fd374c14b2208c84d309
mcmaho62 <mcmaho62@uwm.edu>
2021-03-07 20:01:33 -0600

    fixing some stuff

--- Backend/src/api/admin.py
+++ Backend/src/api/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
+# from api.models import Itinerary, Profile
 
-from api.models import Itinerary, Profile, Token
-
-admin.site.register(Profile)
-admin.site.register(Itinerary)
-admin.site.register(Token)
-
-
-# Register your models here.
+# admin.site.register(Profile)
+# admin.site.register(Itinerary)
