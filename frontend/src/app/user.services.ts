0df16fef4e8af8d168fcaf1030a7c2fa1b18bf09
ParkerCarl <carls363@uwm.edu>
2021-03-09 16:20:36 -0600

    User Service

--- frontend/src/app/user.services.ts
+++ frontend/src/app/user.services.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserService {
 
   constructor(private http: HttpClient){ }
 
-  loginUser(userData){
-
+  loginUser(userData): Observable<any>{
+    return
   }
 
-  registerUser(userData){
-
+  registerUser(userData): Observable<any>{
+    return
   }
 
-  forgotPasswordUser(userData){
-
+  forgotPasswordUser(userData): Observable<any>{
+    return
   }
 
 }
