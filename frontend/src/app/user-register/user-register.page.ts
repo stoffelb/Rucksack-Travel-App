b2c54b29f767e787b77b992857560602a3d1e4b6
ParkerCarl <carls363@uwm.edu>
2021-03-10 12:17:00 -0600

    Register page

--- frontend/src/app/user-register/user-register.page.ts
+++ frontend/src/app/user-register/user-register.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { UserService } from '../user.services';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.page.html',
   styleUrls: ['./user-register.page.scss'],
+  providers: [UserService]
 })
 export class UserRegisterPage implements OnInit {
 
-  constructor() { }
+  first_name;
+  last_name;
+  email;
+  user_name;
+  user_password;
+  user_password_confirm
+
+  constructor(private http: HttpClient, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  registerClick(){
+
+    if(this.user_password === this.user_password_confirm){
+      //Do the register stuff
+    }else{
+      alert("Please make sure passwords match")
+    }
+
+    if(this.first_name && this.last_name && this.email && this.user_name && this.user_password && this.user_password_confirm){
+      //All fields have data
+    }else{
+      alert("Missing input in fields")
+    }
+
   }
 
 }
