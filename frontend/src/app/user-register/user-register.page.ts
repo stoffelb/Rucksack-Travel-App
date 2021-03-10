b6d18447b561d39f83b0afd05d6b9a9e6abbde65
ParkerCarl <carls363@uwm.edu>
2021-03-10 12:49:17 -0600

    Register done, just needs http requets

--- frontend/src/app/user-register/user-register.page.ts
+++ frontend/src/app/user-register/user-register.page.ts
@@ -25,18 +25,36 @@ export class UserRegisterPage implements OnInit {
 
   registerClick(){
 
-    if(this.user_password === this.user_password_confirm){
-      //Do the register stuff
-    }else{
-      alert("Please make sure passwords match")
-    }
-
     if(this.first_name && this.last_name && this.email && this.user_name && this.user_password && this.user_password_confirm){
-      //All fields have data
+      if(this.user_password === this.user_password_confirm){
+
+        //START HTTP REQUEST HERE TO ADD USER TO DJANGO DATABASE
+
+      }else{
+        alert("Please make sure passwords match")
+      }
     }else{
-      alert("Missing input in fields")
+      if(!this.first_name){
+        alert("First Name is missing");
+      }
+      else if(!this.last_name){
+        alert("Last Name is missing");
+      }
+      else if(!this.email){
+        alert("Email is missing");
+      }
+      else if(!this.user_name){
+        alert("Username is missing");
+      }
+      else if(!this.user_password){
+        alert("Password is missing");
+      }
+      else if(!this.user_password_confirm){
+        alert("Confirm password is missing");
+      }
+      else{
+        alert("Make sure you have filled out all fields");
+      }
     }
-
   }
-
 }
