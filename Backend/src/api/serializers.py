91ba0883f67fa61a9da69d32b99fd8e0fe1dcff5
mcmaho62 <mcmaho62@uwm.edu>
2021-03-10 12:30:38 -0600

    accommodation_tag and transportation_tag

--- Backend/src/api/serializers.py
+++ Backend/src/api/serializers.py
@@ -24,4 +24,4 @@ class ItinerarySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Itinerary
-        fields = ('user', 'itinerary_title', 'budget', 'duration_magnitude')
+        fields = ('user', 'title', 'budget', 'duration_magnitude', 'description', 'transportation_tag', 'accommodation_tag')
