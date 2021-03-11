2b093bf2b7a8646e2440d222855849f76acbc409
ParkerCarl <carls363@uwm.edu>
2021-03-10 21:25:53 -0600

    Drop down menus for create itinerary

--- frontend/src/app/main-page/main-page.page.ts
+++ frontend/src/app/main-page/main-page.page.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpParams, JsonpClientBackend } from '@angular/common/http';
 import { UserService } from '../user.services';
-import { Observable } from 'rxjs';
 import { ItineraryObject } from '../ItineraryObject';
-import { Key } from 'selenium-webdriver';
 
 
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -18,7 +16,6 @@ const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo ei
 })
 export class MainPagePage implements OnInit {
   items: ItineraryObject[] = [];
-  its: any[]=[];
 
   constructor(private http: HttpClient, private userService: UserService, private router: Router, private itineraryObject: ItineraryObject) {
 
@@ -33,8 +30,6 @@ export class MainPagePage implements OnInit {
         //Then we can iterate items like normal
 
         this.items = Object.keys(data).map(key => data[key]);
-
-
 
       },
       error => {
